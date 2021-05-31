0f7afcf440bcc72b91edaabd13be7ad8da1bf499
SOULEYSANOGO <SOULEYSANOGO@CIEW15854>
2021-05-31 14:42:33 +0000

    Update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
 
-INSERT INTO userinfo (email,active,last_name,first_name,password,role) values('manager@email.com',true,'Manager','Project','$2a$10$pNOQHgXFKcfrqzdgsPI2oOBJlXaGiAWQqh8fy2sAV9cOtpzfiyBf.','MANAGER');
+INSERT INTO userinfo (email,active,last_name,first_name,password,role) values('manager2@email.com',true,'Manager','Project','$2a$10$pNOQHgXFKcfrqzdgsPI2oOBJlXaGiAWQqh8fy2sAV9cOtpzfiyBf.','MANAGER');
